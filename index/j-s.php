2f0ed6eee0a911f94bc1bf9270135c7f203f8dbc
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-09-08 00:42:17 +0700

    Good Night!

--- index/j-s.php
+++ index/j-s.php
@@ -5,36 +5,46 @@ namespace x\minify {
         if ("" === ($from = \trim($from ?? ""))) {
             return null;
         }
-        $r = '`"\'/' . '!#$%&()*+,-.:;<=>?@[\]^_`{|}~ ' . "\n\r\t";
+        $r = '`"\'/' . '!#$%&()*+,-.:;<=>?@[\]^_`{|}~';
         $to = "";
-        while (false !== ($chop = \strpbrk($from, $r))) {
-            if ("" !== ($v = \strstr($from, $chop[0], true))) {
+        while (false !== ($chop = \strpbrk($from, $r . " \n\r\t"))) {
+            if ("" !== ($v = \strstr($from, $c = $chop[0], true))) {
                 $from = $chop;
-                $to .= j_s\n($v);
+                if ('.' === $c && ('0' === $v || \strspn('0', $v) === \strlen($v))) {
+                    // Remove `0.` prefix
+                } else if ('.' === \substr($to, -1) && false !== \strpos($r . '0123456789', \substr($to, -2, 1))) {
+                    if ("" !== ($v = \rtrim($v, '0'))) {
+                        $to .= $v;
+                    } else {
+                        $to = \substr($to, 0, -1) . '0';
+                    }
+                } else {
+                    $to .= $v;
+                }
             }
-            if (false !== \strpos(" \n\r\t", $chop[0])) {
-                // TODO
-                // echo '<pre style="border:1px solid">';
-                // echo json_encode($chop);
-                // echo '</pre>';
-                $from = \substr($from, 1);
+            if ($n = \strspn($chop, " \n\r\t")) {
+                $from = \substr($from, $n);
+                if ("" !== $from && "" !== $to && false === \strpos($r, $from[0]) && false === \strpos($r, \substr($to, -1))) {
+                    $to .= ' ';
+                }
                 continue;
             }
             if (
-                '`' === $chop[0] && \preg_match('/^`[^`\\\\]*(?>\\\\.[^`\\\\]*)*`/', $chop, $m) ||
-                '"' === $chop[0] && \preg_match('/^"[^"\\\\]*(?>\\\\.[^"\\\\]*)*"/', $chop, $m) ||
-                "'" === $chop[0] && \preg_match('/^\'[^\'\\\\]*(?>\\\\.[^\'\\\\]*)*\'/', $chop, $m)
+                '`' === $c && \preg_match('/^`[^`\\\\]*(?>\\\\.[^`\\\\]*)*`/', $chop, $m) ||
+                '"' === $c && \preg_match('/^"[^"\\\\]*(?>\\\\.[^"\\\\]*)*"/', $chop, $m) ||
+                "'" === $c && \preg_match('/^\'[^\'\\\\]*(?>\\\\.[^\'\\\\]*)*\'/', $chop, $m)
             ) {
                 $from = \substr($from, \strlen($m[0]));
-                if ('`' === $chop[0] && false !== \strpos($m[0], '${')) {
-                    $m[0] = \preg_replace_callback('/\$(\{[^{}\\\\]*(?>\\\\.[^{}\\\\]*)*\})/', static function ($m) {
+                if ('`' === $c && false !== \strpos($m[0], '${')) {
+                    $to .= \preg_replace_callback('/\$(\{[^{}\\\\]*(?>\\\\.[^{}\\\\]*)*\})/', static function ($m) {
                         return j_s($m[0]);
                     }, $m[0]);
+                    continue;
                 }
                 $to .= $m[0];
                 continue;
             }
-            if ('/' === $chop[0]) {
+            if ('/' === $c) {
                 $test = $chop[1] ?? 0;
                 // `/*…*/`
                 if ('*' === $test && \preg_match('/^\/\*[^*]*\*+([^\/*][^*]*\*+)*\//', $chop, $m)) {
@@ -51,7 +61,7 @@ namespace x\minify {
                 }
                 // `//…`
                 if ('/' === $test) {
-                    $from = \substr($from, \strlen(\strstr($chop, "\n", true)));
+                    $from = \substr($from, \strpos($chop, "\n") + 1);
                     continue;
                 }
                 // Look like a regular expression <https://javascript.info/regexp-introduction#flags>
@@ -60,40 +70,29 @@ namespace x\minify {
                     $to .= $m[0];
                     continue;
                 }
-                if (false !== \strpos($r, $chop[0])) {
-                    $from = \ltrim(\substr($from, 1));
-                    $to .= $chop[0];
-                    continue;
-                }
                 $from = \substr($from, 1);
                 $to .= $chop[0];
                 continue;
             }
-            if (false !== \strpos($r, $chop[0])) {
-                $from = \ltrim(\substr($from, 1));
-                if (false !== \strpos('$_', $chop[0]) && \preg_match('/\w/', \substr($to, -1))) {
+            if (false !== \strpos($r, $c)) {
+                $from = \substr($from, 1);
+                if (false !== \strpos('$_', $c) && \preg_match('/\w/', \substr($to, -1))) {
                     // Fix case for `var $asdf` or `var _asdf`
                     $to .= ' ';
-                } else if (false !== \strpos(')]', $chop[0])) {
+                } else if (false !== \strpos(')]', $c)) {
                     $to = \rtrim($to, ',');
-                } else if ('}' === $chop[0]) {
+                } else if ('}' === $c) {
                     $to = \rtrim($to, ';');
                 }
-                $to .= $chop[0];
+                $to .= $c;
                 continue;
             }
             $from = "";
-            $to .= j_s\n($chop);
+            $to .= $chop;
         }
         if ("" !== $from) {
-            $to .= j_s\n($from);
+            $to .= $from;
         }
         return "" !== ($to = \trim($to)) ? $to : null;
-    }
-}
-
-namespace x\minify\j_s {
-    function n(string $from): string {
-        return \trim(\preg_replace('/\s+/', ' ', $from));
     }
 }
