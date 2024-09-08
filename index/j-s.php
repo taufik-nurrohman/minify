5ec689d3d17425ad67f762e01b8f5aa81638825e
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-09-08 09:16:45 +0700

    Untested

--- index/j-s.php
+++ index/j-s.php
@@ -5,26 +5,18 @@ namespace x\minify {
         if ("" === ($from = \trim($from ?? ""))) {
             return null;
         }
-        $r = '`"\'/' . '!#$%&()*+,-.:;<=>?@[\]^_`{|}~';
+        $c1 = '0123456789';
+        $c2 = '`"\'/' . '!#%&()*+,-.:;<=>?@[\]^`{|}~'; // Punctuation(s) but `$` and `_`
+        $c3 = " \n\r\t";
         $to = "";
-        while (false !== ($chop = \strpbrk($from, $r . " \n\r\t"))) {
+        while (false !== ($chop = \strpbrk($from, $c1 . $c2 . $c3))) {
             if ("" !== ($v = \strstr($from, $c = $chop[0], true))) {
                 $from = $chop;
-                if ('.' === $c && ('0' === $v || \strspn('0', $v) === \strlen($v))) {
-                    // Remove `0.` prefix
-                } else if ('.' === \substr($to, -1) && false !== \strpos($r . '0123456789', \substr($to, -2, 1))) {
-                    if ("" !== ($v = \rtrim($v, '0'))) {
-                        $to .= $v;
-                    } else {
-                        $to = \substr($to, 0, -1) . '0';
-                    }
-                } else {
-                    $to .= $v;
-                }
+                $to .= $v;
             }
-            if ($n = \strspn($chop, " \n\r\t")) {
+            if ($n = \strspn($chop, $c3)) {
                 $from = \substr($from, $n);
-                if ("" !== $from && "" !== $to && false === \strpos($r, $from[0]) && false === \strpos($r, \substr($to, -1))) {
+                if ("" !== $from && "" !== $to && false === \strpos($c2, $from[0]) && false === \strpos($c2, \substr($to, -1))) {
                     $to .= ' ';
                 }
                 continue;
@@ -36,7 +28,7 @@ namespace x\minify {
             ) {
                 $from = \substr($from, \strlen($m[0]));
                 if ('`' === $c && false !== \strpos($m[0], '${')) {
-                    $to .= \preg_replace_callback('/\$(\{[^{}\\\\]*(?>\\\\.[^{}\\\\]*)*\})/', static function ($m) {
+                    $to .= \preg_replace_callback('/\$(\{[^}\\\\]*(?>\\\\.[^}\\\\]*)*\})/', static function ($m) {
                         return j_s($m[0]);
                     }, $m[0]);
                     continue;
@@ -74,12 +66,9 @@ namespace x\minify {
                 $to .= $chop[0];
                 continue;
             }
-            if (false !== \strpos($r, $c)) {
+            if (false !== \strpos($c2, $c)) {
                 $from = \substr($from, 1);
-                if (false !== \strpos('$_', $c) && \preg_match('/\w/', \substr($to, -1))) {
-                    // Fix case for `var $asdf` or `var _asdf`
-                    $to .= ' ';
-                } else if (false !== \strpos(')]', $c)) {
+                if (false !== \strpos(')]', $c)) {
                     $to = \rtrim($to, ',');
                 } else if ('}' === $c) {
                     $to = \rtrim($to, ';');
