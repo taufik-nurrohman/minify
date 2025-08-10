82b7b737f4b52fdc85e7b72424f36f37b6562c60
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-08-10 22:12:09 +0700

    Thanks ChatGPT!

--- index/j-s-o-n.php
+++ index/j-s-o-n.php
@@ -8,33 +8,39 @@ namespace x\minify {
         if ('""' === $from || '[]' === $from || 'false' === $from || 'null' === $from || 'true' === $from || '{}' === $from || \is_numeric($from)) {
             return $from;
         }
-        $c1 = ',:[]{}';
+        $count = \strlen($from);
+        $i = 0;
         $to = "";
-        while (false !== ($chop = \strpbrk($from, '"' . $c1))) {
-            if ("" !== ($v = \strstr($from, $c = $chop[0], true))) {
-                $from = $chop;
-                $to .= \trim($v);
-            }
-            if (false !== \strpos($c1, $c)) {
-                $from = \substr($from, 1);
-                $to .= $chop[0];
+        while ($i < $count) {
+            $c = $from[$i];
+            if (' ' === $c || "\n" === $c || "\r" === $c || "\t" === $c) {
+                $i++;
                 continue;
             }
-            if ('""' === \substr($chop, 0, 2)) {
-                $from = \substr($from, 2);
-                $to .= '""';
+            if ('"' === $c) {
+                $i++;
+                $to .= $c;
+                while ($i < $count) {
+                    if ('"' === ($c = $from[$i])) {
+                        $j = $i - 1;
+                        $x = 0;
+                        while ($j >= 0 && "\\" === $from[$j]) {
+                            $j--;
+                            $x++;
+                        }
+                        if (0 === $x % 2) {
+                            $i++;
+                            $to .= $c;
+                            break;
+                        }
+                    }
+                    $i++;
+                    $to .= $c;
+                }
                 continue;
             }
-            if ('"' === $c && \preg_match('/^"[^"\\\\]*(?>\\\\.[^"\\\\]*)*"/', $chop, $m)) {
-                $from = \substr($from, \strlen($m[0]));
-                $to .= $m[0];
-                continue;
-            }
-            $from = "";
-            $to .= \trim($chop); // `false`, `null`, `true`, `1`, `1.0`
-        }
-        if ("" !== $from) {
-            $to .= \trim($from);
+            $i++;
+            $to .= $c;
         }
         return "" !== ($to = \trim($to)) ? $to : null;
     }
